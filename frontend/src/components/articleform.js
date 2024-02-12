48924cfd494715982a7e31fced5933ad49713039
Maxime Yene <maximeyene@gmail.com>
2024-02-12 14:33:29 +0100

    wip

--- frontend/src/components/articleform.js
+++ frontend/src/components/articleform.js
@@ -1,42 +1,60 @@
 import { useState } from "react";
+import axios from "axios";
 
 const ArticleFormular = () => {
-    const [name,setName]=useState('');
+    const [title,setTitle]=useState('');
     const [price,setPrice]=useState('');
     const [category,setCategory]=useState('');
     const [imageFile,setImageFile]=useState(null)
 
-    const handleFormClear=()=>{
-        setName('');
-        setPrice('');
-        setCategory('');
-    }
+    // const handleClearForm=()=>{
+    //     setTitle('');
+    //     setPrice('');
+    //     setCategory('');
+    // }
 
-    const handleSubmit=()=>{
-        handleFormClear();
+    const handleSubmit= async()=>{
+        const formData= new FormData();
+        formData.append('title',title);
+        formData.append('price',price);
+        formData.append('category',category);
+        formData.append('imageFile',imageFile);
+
+        try{
+            await axios.post('http://localhost:3000/api/uploadArticle',formData, {
+                headers:{
+                    'Content-Type':'multipart/form-data'
+                }
+            });
+            console.log('Fichier téléchargé avec succès');
+        }catch(error){
+            console.error("Erreur lors du téléchargement du fichier", error);
+        }
+        // handleClearForm();
+        // e.preventDefault();
     }
 
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit()} >
                 <div>
                     <div>
                         Nom de l'article:<br />
-                        <input type='text' value={name} placeholder='Entrez le nom du produit à ajouter' required />
+                        <input type='text' value={title} onChange={e=>setTitle(e.target.value)} placeholder='Entrez le nom du produit à ajouter' required />
                     </div>
                     <div>
                         Prix de l'article:<br />
-                        <input type='number' value={price} placeholder='Entrez le prix du produit à ajouter' required />
+                        <input type='text' value={price} onChange={e=>setPrice(e.target.value)} placeholder='Entrez le prix du produit à ajouter' required />
                     </div>
                     <div>
                         Catégorie de l'article:<br />
-                        <input type='text' value={category} placeholder='Entrez la catégorie du produit à ajouter' required />
+                        <input type='text' value={category} onChange={e=>setCategory(e.target.value)} placeholder='Entrez la catégorie du produit à ajouter' required />
                     </div>
                     <div>
-                        <input type='file' placeholder='Insérer une image' required />
+                        <input type='file' onChange={e=>setImageFile(e.target.files[0])} placeholder='Insérer une image' required />
                     </div>
                 </div>
-                <button onClick={handleSubmit()} className="articleForm-button" >Ajouter un article</button>
+                <button className="articleForm-button" >Ajouter un article</button>
             </form>
         </div>
     )
